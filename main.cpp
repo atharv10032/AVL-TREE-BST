c7fdd1e18ec181e4735a002a631f92083a9ab658
kazuma1469 <2022meb1301@iitrpr.ac.in>
2024-09-25 13:31:12 +0530

    general layout of the data structure and working on some utility functions

--- main.cpp
+++ main.cpp
@@ -2,6 +2,61 @@
 
 using namespace std;
 
+class avl_node
+{
+public:
+    using node = avl_node;
+    node *left, *right;
+    int val, height;
+    avl_node(int val)
+    {
+        this->val = val;
+        height = 0;
+        left = right = nullptr;
+    }
+    node *insert(node *root, int key);
+    node *delete_key(node *root, int key);
+    node *search(node *root, int key);
+    void InOrder(node *root);
+    void PreOrder(node *root);
+    void PostOrder(node *root);
+    int height_of_tree(node *root);
+};
+
+int avl_node::height_of_tree(node *root)
+{
+    if (!root)
+        return 0;
+    else
+        return 1 + max(height_of_tree(root->left), height_of_tree(root->right));
+}
+
+avl_node ::node *avl_node::insert(node *root, int key)
+{
+    if (root == nullptr)
+        return new avl_node(key);
+}
+
+class bst_node
+{
+public:
+    using node = bst_node;
+    bst_node *left, *right;
+    int val;
+    bst_node(int val)
+    {
+        this->val = val;
+        left = right = nullptr;
+    }
+    node *insert(node *root, int key);
+    node *delete_key(node *root, int key);
+    node *search(node *root, int key);
+    void InOrder(node *root);
+    void PreOrder(node *root);
+    void PostOrder(node *root);
+    int height_of_tree(node *root);
+};
+
 int main()
 {
 }
