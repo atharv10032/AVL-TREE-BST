524457dc94adb94798e7c8deb1d2156bc4f1f2bb
kazuma1469 <2022meb1301@iitrpr.ac.in>
2024-09-25 18:35:29 +0530

    Working on insert

--- main.cpp
+++ main.cpp
@@ -21,7 +21,20 @@ public:
     void PreOrder(node *root);
     void PostOrder(node *root);
     int height_of_tree(node *root);
+    int heavy(node *root);
 };
+
+int avl_node::heavy(avl_node ::node *root)
+{
+    if (!root)
+        return 0;
+    int r = -1, l = -1;
+    if (root->right)
+        r = root->right->height;
+    if (root->left)
+        l = root->left->height;
+    return r - l;
+}
 
 int avl_node::height_of_tree(node *root)
 {
@@ -35,6 +48,13 @@ avl_node ::node *avl_node::insert(node *root, int key)
 {
     if (root == nullptr)
         return new avl_node(key);
+    if (root->val > key)
+        root->right = insert(root->right, key);
+    else if (root->val < key)
+        root->left = insert(root->left, key);
+    else
+        return root;
+    root->height = height_of_tree(root);
 }
 
 class bst_node
