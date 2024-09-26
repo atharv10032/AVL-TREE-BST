5dced522fb92fc3480715f6136e9aaed4e952339
kazuma1469 <2022meb1301@iitrpr.ac.in>
2024-09-26 12:31:13 +0530

    Finished insert and deletion in avl

--- main.cpp
+++ main.cpp
@@ -22,7 +22,54 @@ public:
     void PostOrder(node *root);
     int height_of_tree(node *root);
     int heavy(node *root);
+    node *leftRotation(node *root);
+    node *rightRotation(node *root);
+    node *successor(node *root);
 };
+
+avl_node::node *avl_node::successor(node *root)
+{
+    if (!root->left)
+        return root;
+    return successor(root->left);
+}
+avl_node::node *avl_node::leftRotation(avl_node::node *root)
+{
+
+    avl_node *y = root->left;
+    avl_node *x = root;
+    avl_node *z = y->left;
+
+    node *B = y->right;
+    y->right = x;
+    x->left = B;
+    if (x)
+        x->height = height_of_tree(x);
+    if (y)
+        y->height = height_of_tree(y);
+    if (z)
+        z->height = height_of_tree(z);
+
+    return y;
+}
+
+avl_node::node *avl_node::rightRotation(avl_node::node *root)
+{
+
+    avl_node *x = root;
+    avl_node *y = x->right;
+    avl_node *z = y->right;
+
+    node *B = y->left;
+    y->left = x;
+    x->right = B;
+
+    x->height = height_of_tree(x);
+    y->height = height_of_tree(y);
+    z->height = height_of_tree(z);
+
+    return y;
+}
 
 int avl_node::heavy(avl_node ::node *root)
 {
@@ -44,6 +91,50 @@ int avl_node::height_of_tree(node *root)
         return 1 + max(height_of_tree(root->left), height_of_tree(root->right));
 }
 
+avl_node::node *avl_node::delete_key(node *root, int key)
+{
+    if (!root)
+        return root;
+
+    if (key < root->val)
+        root->left = delete_key(root->left, key);
+    else if (key > root->val)
+        root->right = delete_key(root->right, key);
+    else
+    {
+        if (!root->left && !root->right)
+            return nullptr;
+        if (!root->right)
+            return root->left;
+        if (!root->left)
+            return root->right;
+        node *succ = successor(root->right);
+        root->val = succ->val;
+        root->right = delete_key(root->right, succ->val);
+        return root;
+    }
+
+    root->height = height_of_tree(root);
+
+    int bal = heavy(root);
+
+    if (bal > 1 && heavy(root->right) >= 0)
+        return leftRotation(root);
+    else if (bal < -1 && heavy(root->left) <= 0)
+        return rightRotation(root);
+    else if (bal > 1 && heavy(root->right) < 0)
+    {
+        root->right = rightRotation(root->right);
+        return leftRotation(root);
+    }
+    else if (bal < -1 && heavy(root->left) > 0)
+    {
+        root->left = leftRotation(root->left);
+        return rightRotation(root);
+    }
+
+    return root;
+}
 avl_node ::node *avl_node::insert(node *root, int key)
 {
     if (root == nullptr)
@@ -55,6 +146,27 @@ avl_node ::node *avl_node::insert(node *root, int key)
     else
         return root;
     root->height = height_of_tree(root);
+
+    int bal = heavy(root);
+
+    if (bal < -1 && key < root->left->val)
+        return leftRotation(root);
+
+    if (bal > 1 && key > root->right->val)
+        return rightRotation(root);
+    if (bal < -1 && key > root->left->val)
+    {
+        root->left = leftRotation(root->left);
+        return rightRotation(root);
+    }
+
+    if (bal > 1 && key < root->left->val)
+    {
+        root->right = rightRotation(root->right);
+        return leftRotation(root);
+    }
+
+    return root;
 }
 
 class bst_node
